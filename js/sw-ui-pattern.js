73c0ecadc26790b35146bffdfb4733cf6fb91f90
talehhasan <talehhasan@gmail.com>
2022-02-05 18:20:46 +0400

    Layout Elements

---
+++ js/sw-ui-pattern.js
@@ -0,0 +1,12 @@
+// ***** Table recipe START ***** //  
+$(document).on("click",'.sw-table-recipe .sw-first-list-item-table', function (e) {
+    $(this).toggleClass('show');
+    $(this).next().toggleClass('show');
+    $(this).find('.sw-table-arrow i.fas').toggleClass('fa-chevron-up');
+});
+$(document).on("click",'.sw-table-recipe .sw-dedail-second-tr-btn', function (e) {
+    $(this).toggleClass('show');
+    $(this).next().toggleClass('show');
+    $(this).find('.sw-table-arrow i.fas').toggleClass('fa-chevron-up');
+});
+// ***** Table recipe FINISH ***** //    
