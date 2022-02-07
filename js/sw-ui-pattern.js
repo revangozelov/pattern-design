575130acb8d2b0938528ff0be539250239b84758
talehhasan <talehhasan@gmail.com>
2022-02-07 19:02:36 +0400

    selectpicker class

--- js/sw-ui-pattern.js
+++ js/sw-ui-pattern.js
@@ -3,10 +3,12 @@ $(document).on("click",'.sw-table-recipe .sw-first-list-item-table', function (e
     $(this).toggleClass('show');
     $(this).next().toggleClass('show');
     $(this).find('.sw-table-arrow i.fas').toggleClass('fa-chevron-up');
+    $(this).closest('tbody').find('select.my-selectpicker').selectpicker('refresh');
 });
 $(document).on("click",'.sw-table-recipe .sw-detail-second-tr-btn', function (e) {
     $(this).toggleClass('show');
     $(this).next().toggleClass('show');
     $(this).find('.sw-table-arrow i.fas').toggleClass('fa-chevron-up');
+    $(this).closest('tbody').find('select.my-selectpicker').selectpicker('refresh');
 });
 // ***** Table recipe FINISH ***** //    
