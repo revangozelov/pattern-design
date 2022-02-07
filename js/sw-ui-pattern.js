2b6dd9bf0ab1f4a1fcb0bcd64621ef90f8de69f5
talehhasan <talehhasan@gmail.com>
2022-02-07 16:17:47 +0400

    asasas

--- js/sw-ui-pattern.js
+++ js/sw-ui-pattern.js
@@ -4,7 +4,7 @@ $(document).on("click",'.sw-table-recipe .sw-first-list-item-table', function (e
     $(this).next().toggleClass('show');
     $(this).find('.sw-table-arrow i.fas').toggleClass('fa-chevron-up');
 });
-$(document).on("click",'.sw-table-recipe .sw-dedail-second-tr-btn', function (e) {
+$(document).on("click",'.sw-table-recipe .sw-detail-second-tr-btn', function (e) {
     $(this).toggleClass('show');
     $(this).next().toggleClass('show');
     $(this).find('.sw-table-arrow i.fas').toggleClass('fa-chevron-up');
